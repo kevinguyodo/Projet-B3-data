752ab085762c4920483da8de94f520ab269f752a
kevinguyodo <kevin.guyodo@ynov.com>
2023-04-18 16:33:51 +0200

    add original method for initiale csv

--- script/clean_db.py
+++ script/clean_db.py
@@ -2,7 +2,7 @@ import sqlite3
 from pathlib import Path
 import pandas as pd
 
-class Connection:
+class CleanDBConnection:
     def __init__(self, conn):
         self.conn = conn
         pass
@@ -10,7 +10,6 @@ class Connection:
     # Create db file in data folder, if it doesn't exist
     def init_db(self):
         open("./data/clean_db.db", "w")
-        open("./data/initial_db.db", "w")
 
     def connect(self):
         self.init_db()
@@ -24,7 +23,13 @@ class Connection:
         # Read CSV file, separate data to format their, and skip first rows => columns1, columns2 ...
         drug_data_csv = pd.read_csv('./data/Drug_clean.csv', sep = ';', skiprows=[0])
         # Insert data in Database
-        drug_data_csv.to_sql('drug', con=db_new_conn, if_exists= 'append')
+        drug_data_csv.to_sql('cleandrug', con=db_new_conn, if_exists= 'append')
         # First select request
-        all_data = db_new_conn.execute('''SELECT * FROM drug''').fetchall()
+        all_data = db_new_conn.execute('''SELECT * FROM cleandrug''').fetchall()
         print(all_data)
+
+    def get_description(self):
+        init_db_data = self.connect()
+        init_db_description = init_db_data.execute('''SELECT Description FROM cleandrug WHERE Description IS NOT NULL''').fetchall()
+        print(init_db_description)
+        pass
