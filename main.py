5cdeaca395e12d248516d53fa6a1e57992d9aab4
kevinguyodo <kevin.guyodo@ynov.com>
2023-04-18 11:23:52 +0200

    Copy data from csv to database

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from script.connection import Connection
 
 
+
 def main():
     new_conn= Connection("Hello World")
-    new_conn.connect()
+    new_conn.get_data_from_csv()
 
 
 main()
--- script/connection.py
+++ script/connection.py
@@ -1,6 +1,6 @@
 import sqlite3
 from pathlib import Path
-import panda
+import pandas as pd
 
 class Connection:
     def __init__(self, conn):
@@ -12,15 +12,23 @@ class Connection:
         open("./data/clean_db.db", "w")
         open("./data/initial_db.db", "w")
 
-
     def connect(self):
         self.init_db()
         # Connect to database
         new_conn = sqlite3.connect("./data/clean_db.db")
         conn = new_conn.cursor()
-        print("Connect data")
+        return conn, new_conn
 
+    def get_data_from_csv(self):
+        db_conn, db_new_conn = self.connect()
+        users = pd.read_csv('./data/Drug_clean.csv', nrows=15)
+        users.to_sql('drug', con=db_new_conn)
+        my_data = db_new_conn.execute('''SELECT * FROM drug''').fetchall()
+        print(my_data[1])
+        # print(db_new_conn.execute('''SELECT * FROM drug''').fetchall())
+        # print(users.head())
 
+        # users.head()
     # def disconnect(self):
     #     sqlite3.
     #     pass
