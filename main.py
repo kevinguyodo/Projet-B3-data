3884bd491091adad4dfee777f1c5dfeb79fa9a03
k4701,K0717 <kevin.guyodo@ynov.com>
2023-05-16 12:40:35 +0200

    last clean database added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from script.clean_db import CleanDBConnection
 from script.initial_db import InitialDBConnection
+from script.final_db import FinalDBConnection
+import os
 
 def main():
     clean_db_conn = CleanDBConnection()
@@ -7,5 +9,9 @@ def main():
 
     initial_db_conn = InitialDBConnection()
     initial_db_conn.get_data_from_csv()
+
+    if os.path.exists("data/final_drug.csv"):
+        final_db_conn = FinalDBConnection()
+        final_db_conn.get_data_from_csv()
 
 main()
