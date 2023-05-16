2c3da589677006b557b93a2e98563588577fa166
k4701,K0717 <kevin.guyodo@ynov.com>
2023-05-16 11:47:37 +0200

    update db script

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from script.connection import Connection
-
-
+from script.clean_db import CleanDBConnection
+from script.initial_db import InitialDBConnection
 
 def main():
-    new_conn= Connection("Hello World")
-    new_conn.get_data_from_csv()
+    clean_db_conn = CleanDBConnection()
+    clean_db_conn.get_data_from_csv()
 
+    initial_db_conn = InitialDBConnection()
+    initial_db_conn.get_data_from_csv()
 
 main()
